c045018bab7da6589e5628307abcb0c89b4a959e
Nia Smith <niasmith94@outlook.com>
2020-08-08 15:10:17 -0400

    need to reshuffle the code, but this is what's needed.

--- myGuessingGame.py
+++ myGuessingGame.py
@@ -1,4 +1,27 @@
+
 print("Hello, welcome to my game!")
-value = input("What is your name?")
-print(value)
-print('Welcome'+'name'+'!')
+value = raw_input("What is your name?")
+print("Let us play a game!")
+print("This is a number guessing games")
+
+import random
+
+hidden = random.randrange(1, 201)
+print(hidden)
+
+guess = int(input("Please guess a number"))
+
+if guess == hidden:
+    print("Correct answer!")
+elif guess < hidden:
+    print("You're too low")
+else:
+    print("You're too high")
+
+# while loop
+# index = 0
+# while True:
+#     print(index)
+#     index += 0
+#     if index < 10:
+#         break;
